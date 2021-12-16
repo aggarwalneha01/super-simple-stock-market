2b70c162acda8da92ea5329114770ec2a28178b2
Neha Aggarwal <neha.aggarwal@bbc.co.uk>
2021-12-16 01:35:06 +0000

    display values up to 2 digits only

--- src/helpers/get-values.js
+++ src/helpers/get-values.js
@@ -1,15 +1,15 @@
 
 export const getDividendYield = (type, lastDividend, fixedDividend, price, parValue) => {
     if (type === "Common") {
-            return lastDividend / price;
+            return (lastDividend / price).toFixed(2);
     } else if (type === "Preferred") {
-            return fixedDividend * parValue / price;
+            return (fixedDividend * parValue / price).toFixed(2);
     }
     return null;
 };
 
 export const getPERatio = (dividendYield, price) => {
-        return price / dividendYield;
+        return (price / dividendYield).toFixed(2);
 };
 
 export const getVWSPrice = (trades) => {
@@ -19,7 +19,7 @@ export const getVWSPrice = (trades) => {
         VWSPriceDividend += parseInt(trade.stockPrice) * parseInt(trade.stockQuantity);
         VSWPriceDivisor += parseInt(trade.stockQuantity); 
     }
-    return VWSPriceDividend / VSWPriceDivisor;
+    return (VWSPriceDividend / VSWPriceDivisor).toFixed(2);
 }
 
 export const getGeometricMean = (trades) =>{
@@ -27,5 +27,5 @@ export const getGeometricMean = (trades) =>{
     for (let trade of trades){
         geometricMean *= trade.stockPrice;
     }
-    return Math.pow(geometricMean, 1 / trades.length); 
+    return Math.pow(geometricMean, 1 / trades.length).toFixed(2); 
 }
